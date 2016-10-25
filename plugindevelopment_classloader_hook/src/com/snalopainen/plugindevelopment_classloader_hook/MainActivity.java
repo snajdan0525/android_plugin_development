4d66ce2779c599247c5c0d237b67c94c191c5580
snajdan890525 <435063994@qq.com>
2016-10-25 22:49:35 +0800

    patch classloader by combine dexElements[]

--- plugindevelopment_classloader_hook/src/com/snalopainen/plugindevelopment_classloader_hook/MainActivity.java
+++ plugindevelopment_classloader_hook/src/com/snalopainen/plugindevelopment_classloader_hook/MainActivity.java
@@ -1,5 +1,7 @@
 package com.snalopainen.plugindevelopment_classloader_hook;
 
+import java.lang.reflect.Field;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,5 +13,32 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	private static Object getPathList(Object baseDexClassLoader)
+			throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException, ClassNotFoundException {
+		return getField(baseDexClassLoader, Class.forName("dalvik.system.BaseDexClassLoader"), "pathList");
+	}
+	
+	private static Object getField(Object obj, Class<?> cl, String field)
+			throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+		Field localField = cl.getDeclaredField(field);
+		localField.setAccessible(true);
+		return localField.get(obj);
+	}
+	
+	private static Object getDexElements(Object paramObject)
+			throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
+		return getField(paramObject, paramObject.getClass(), "dexElements");
+	}
+	
+	//setField(pathList, pathList.getClass(), "dexElements", dexElements);
+	private static void setField(Object obj, Class<?> cl, String field,
+			Object value) throws NoSuchFieldException,
+			IllegalArgumentException, IllegalAccessException {
+
+		Field localField = cl.getDeclaredField(field);
+		localField.setAccessible(true);
+		localField.set(obj, value);
 	}
 }
