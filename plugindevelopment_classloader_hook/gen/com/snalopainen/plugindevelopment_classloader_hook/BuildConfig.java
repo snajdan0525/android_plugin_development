37b2baea019bbe75d4666d5c63e720d23a204925
snajdan890525 <435063994@qq.com>
2016-10-25 20:21:06 +0800

    add project classloader_hook

---
+++ plugindevelopment_classloader_hook/gen/com/snalopainen/plugindevelopment_classloader_hook/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.snalopainen.plugindevelopment_classloader_hook;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
