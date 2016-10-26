b04e190f3d9ceb85ccbedf863a0722ff6949010a
snajdan890525 <435063994@qq.com>
2016-10-26 18:05:30 +0800

    replace target intent with stub intent

--- plugin_development_cheat_ams/src/com/snalopainen/plugin_development_cheat_ams/AMSHook.java
+++ plugin_development_cheat_ams/src/com/snalopainen/plugin_development_cheat_ams/AMSHook.java
@@ -20,32 +20,44 @@ public class AMSHook implements InvocationHandler {
 		// 拦截startActivity方法
 		if ("startActivity".equals(method.getName())) {
 			// 查找原始的intent对象
-			Intent raw = null;
-			final int size = (args == null ? 0 : args.length);
-			int i = 0;
-			for (; i < size; ++i) {
-				if (args[i] instanceof Intent) {
-					raw = (Intent) args[i];
-					break;
+			
+			int intentOfArgIndex = findFirstIntentIndexArgs(args);
+			if (intentOfArgIndex >= 0) {
+				Intent raw = null;
+				raw = (Intent) args[intentOfArgIndex];
+				// 看下是否是启动插件中的activity
+				if (raw.getBooleanExtra(Constants.EXTRA_INVOKE_PLUGIN, false)) {
+					// 获得原始的ComponentName
+					ComponentName componentName = raw.getComponent();
+					// 创建一个新的Intent
+					Intent intent = new Intent();
+					// 把Component替换为StubActivity的 这样就不会被系统检测到
+					// 启动一个没有在AndroidManifest.xml//中声明的activity
+					intent.setComponent(new ComponentName(componentName
+							.getPackageName(), SubActivity.class
+							.getCanonicalName()));
+					// 保存原始的intent
+					intent.putExtra(Constants.EXTRA_RAW_INTENT, raw);
+					// 替换为新的Intent
+					args[intentOfArgIndex] = intent;
 				}
-			}
-			// 看下是否是启动插件中的activity
-			if (raw.getBooleanExtra(Constants.EXTRA_INVOKE_PLUGIN, false)) {
-				// 获得原始的ComponentName
-				ComponentName componentName = raw.getComponent();
-				// 创建一个新的Intent
-				Intent intent = new Intent();
-				// 把Component替换为StubActivity的 这样就不会被系统检测到
-				// 启动一个没有在AndroidManifest.xml//中声明的activity
-				intent.setComponent(new ComponentName(componentName
-						.getPackageName(), SubActivity.class.getCanonicalName()));
-				// 保存原始的intent
-				intent.putExtra(Constants.EXTRA_RAW_INTENT, raw);
-				// 替换为新的Intent
-				args[i] = intent;
 			}
 		}
 		// 还是按往常一样调用各种函数
 		return method.invoke(m_base, args);
 	}
+
+	private static int findFirstIntentIndexArgs(Object[] args) {
+		if (args != null && args.length > 0) {
+			int i = 0;
+			for (Object object : args) {
+				if (object instanceof Intent) {
+					return i;
+				}
+				i++;
+			}
+		}
+		return -1;
+	}
+
 }
